987c9e8d3a68f560080b0f424a0b96216d8c3fb6
peichtoSICKAG <115688562+peichtoSICKAG@users.noreply.github.com>
2023-12-13 10:19:12 +0100

    public release 2.3.0

--- src/VisionaryAutoIPScan.cpp
+++ src/VisionaryAutoIPScan.cpp
@@ -45,7 +45,12 @@ namespace visionary {
 
 namespace pt = boost::property_tree;
 
-const std::string VisionaryAutoIPScan::DEFAULT_BROADCAST_ADDR{"255.255.255.255"};
+const std::string  VisionaryAutoIPScan::DEFAULT_BROADCAST_ADDR{"255.255.255.255"};
+const std::string  VisionaryAutoIPScan::DEFAULT_IP_MASK{"255.255.255.0"};
+const std::string  VisionaryAutoIPScan::DEFAULT_GATEWAY{"0.0.0.0"};
+const std::uint8_t kRplIpconfig  = 0x91; // replied by sensor; confirmation to IP change
+const std::uint8_t kRplNetscan   = 0x95; // replied by sensors; with information like device name, serial number, IP,...
+const std::uint8_t kRplScanColaB = 0x90; // replied by sensors; with information like device name, serial number, IP,...
 
 std::vector<VisionaryAutoIPScan::DeviceInfo> VisionaryAutoIPScan::doScan(unsigned           timeOut,
                                                                          const std::string& broadcastAddress,
@@ -70,7 +75,7 @@ std::vector<VisionaryAutoIPScan::DeviceInfo> VisionaryAutoIPScan::doScan(unsigne
   autoIpPacket.push_back(0x0);  // reserved
   // length of datablock
   autoIpPacket.push_back(0x0);
-  autoIpPacket.push_back(0x0);
+  autoIpPacket.push_back(0x08);
   // Mac address
   autoIpPacket.push_back(0xFF);
   autoIpPacket.push_back(0xFF);
@@ -83,9 +88,18 @@ std::vector<VisionaryAutoIPScan::DeviceInfo> VisionaryAutoIPScan::doScan(unsigne
   autoIpPacket.push_back(0x0);
   autoIpPacket.push_back(0x0);
   autoIpPacket.push_back(0x0);
-  // reserved
+
+  // Additional UCP Scan information
+  autoIpPacket.push_back(0x01); // Indicates that telegram is a CoLa Scan telegram
   autoIpPacket.push_back(0x0);
-  autoIpPacket.push_back(0x0);
+
+  // serverIP
+  std::vector<std::uint8_t> ipAddrVector = convertIpToBinary(m_serverIP);
+  autoIpPacket.insert(autoIpPacket.end(), ipAddrVector.begin(), ipAddrVector.end());
+
+  // server net mask
+  std::vector<std::uint8_t> maskVector = convertIpToBinary(m_serverNetMask);
+  autoIpPacket.insert(autoIpPacket.end(), maskVector.begin(), maskVector.end());
 
   // Replace telegram id in packet
   unsigned int curtelegramID = teleIdCounter++;
@@ -102,43 +116,50 @@ std::vector<VisionaryAutoIPScan::DeviceInfo> VisionaryAutoIPScan::doScan(unsigne
     const std::chrono::steady_clock::time_point now(std::chrono::steady_clock::now());
     if ((now - startTime) > std::chrono::milliseconds(timeOut))
     {
+      std::cout << __FUNCTION__ << " Timeout" << '\n';
       break;
     }
     if (pTransport->recv(receiveBuffer, 1400) > 16) // 16 bytes minsize
     {
       unsigned int pos = 0;
-      if (receiveBuffer[pos++] != 0x90) // 0x90 = answer package id and 16 bytes minsize
+      if (receiveBuffer[0] == kRplNetscan)
       {
+        DeviceInfo dI = parseAutoIPBinary(receiveBuffer);
+        deviceList.push_back(dI);
         continue;
       }
-      pos += 1; // unused byte
-      std::uint16_t payLoadSize = readUnalignBigEndian<std::uint16_t>(receiveBuffer.data() + pos);
-      pos += 2;
-      pos += 6; // Skip mac address(part of xml)
-      std::uint32_t recvTelegramID = readUnalignBigEndian<std::uint32_t>(receiveBuffer.data() + pos);
-      pos += 4;
-      // check if it is a response to our scan
-      if (recvTelegramID != curtelegramID)
+      if (receiveBuffer[0] == kRplScanColaB)
       {
-        continue;
-      }
-      pos += 2; // unused
-      // Get XML Payload
-      if (receiveBuffer.size() >= pos + payLoadSize)
-      {
-        std::stringstream stringStream(std::string(reinterpret_cast<char*>(&receiveBuffer[pos]), payLoadSize));
-        try
+        pos++;
+        pos += 1; // unused byte
+        std::uint16_t payLoadSize = readUnalignBigEndian<std::uint16_t>(receiveBuffer.data() + pos);
+        pos += 2;
+        pos += 6; // Skip mac address(part of xml)
+        std::uint32_t recvTelegramID = readUnalignBigEndian<std::uint32_t>(receiveBuffer.data() + pos);
+        pos += 4;
+        // check if it is a response to our scan
+        if (recvTelegramID != curtelegramID)
         {
-          DeviceInfo dI = parseAutoIPXml(stringStream);
-          deviceList.push_back(dI);
+          continue;
         }
-        catch (...)
+        pos += 2; // unused
+        // Get XML Payload
+        if (receiveBuffer.size() >= pos + payLoadSize)
         {
+          std::stringstream stringStream(std::string(reinterpret_cast<char*>(&receiveBuffer[pos]), payLoadSize));
+          try
+          {
+            DeviceInfo dI = parseAutoIPXml(stringStream);
+            deviceList.push_back(dI);
+          }
+          catch (...)
+          {
+          }
         }
-      }
-      else
-      {
-        std::cout << __FUNCTION__ << "Received invalid AutoIP Packet" << std::endl;
+        else
+        {
+          std::cout << __FUNCTION__ << "Received invalid AutoIP Packet" << '\n';
+        }
       }
     }
   }
@@ -185,7 +206,7 @@ VisionaryAutoIPScan::DeviceInfo VisionaryAutoIPScan::parseAutoIPXml(std::strings
   DeviceInfo dI;
   dI.deviceName        = deviceType;
   dI.ipAddress         = ipAddress;
-  dI.macAddress        = macAddress;
+  dI.macAddress        = convertMacToStruct(macAddress);
   std::size_t   idx    = 0u;
   unsigned long portul = std::stoul(port, &idx);
   if ((idx < port.length()) || (portul > std::numeric_limits<std::uint16_t>::max()) || (portul == 0u))
@@ -202,6 +223,401 @@ VisionaryAutoIPScan::DeviceInfo VisionaryAutoIPScan::parseAutoIPXml(std::strings
   dI.subNet = subNet;
 
   return dI;
+}
+
+VisionaryAutoIPScan::DeviceInfo VisionaryAutoIPScan::parseAutoIPBinary(const VisionaryAutoIPScan::ByteBuffer& buffer)
+{
+  DeviceInfo deviceInfo;
+
+  deviceInfo.subNet       = "";
+  deviceInfo.port         = 0;
+  deviceInfo.protocolType = COLA_2;
+
+  int offset = 16;
+  //  auto deviceInfoVersion = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+
+  auto cidNameLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+  std::string cidName;
+  for (int i = 0; i < cidNameLen; ++i)
+  {
+    cidName += readUnalignBigEndian<char>(buffer.data() + offset);
+    offset++;
+  }
+  deviceInfo.deviceName = cidName;
+
+  //  auto cidMajorVersion = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+  //  auto cidMinorVersion = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+  //  auto cidPatchVersion = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+  //  auto cidBuildVersion = readUnalignBigEndian<std::uint32_t>(buffer.data() + offset);
+  offset += 4;
+  //  auto cidVersionClassifier = readUnalignBigEndian<std::uint8_t>(buffer.data() + offset);
+  offset += 1;
+
+  //  auto deviceState = readUnalignBigEndian<char>(buffer.data() + offset);
+  offset += 1;
+
+  //  auto reqUserAction = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+
+  // Device name
+  auto deviceNameLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+  std::string deviceName(reinterpret_cast<const char*>(buffer.data() + offset), deviceNameLen);
+  offset += deviceNameLen;
+
+  // App name
+  auto appNameLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+  std::string appName(reinterpret_cast<const char*>(buffer.data() + offset), appNameLen);
+  offset += appNameLen;
+
+  // Project name
+  auto projNameLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+  std::string projName(reinterpret_cast<const char*>(buffer.data() + offset), projNameLen);
+  offset += projNameLen;
+
+  // Serial number
+  auto serialNumLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+  std::string serialNum(reinterpret_cast<const char*>(buffer.data() + offset), serialNumLen);
+  offset += serialNumLen;
+
+  // Type code
+  auto typeCodeLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+  std::string typeCode(reinterpret_cast<const char*>(buffer.data() + offset), typeCodeLen);
+  offset += typeCodeLen;
+
+  // Firmware version
+  auto firmwareVersionLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+  std::string firmwareVersion(reinterpret_cast<const char*>(buffer.data() + offset), firmwareVersionLen);
+  offset += firmwareVersionLen;
+
+  // Order number
+  auto orderNumberLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+  std::string orderNumber(reinterpret_cast<const char*>(buffer.data() + offset), orderNumberLen);
+  offset += orderNumberLen;
+
+  // # unused: flags, = struct.unpack('>B', rpl[offset:offset + 1])
+  offset += 1;
+
+  auto auxArrayLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+
+  for (int i = 0; i < auxArrayLen; ++i)
+  {
+    std::string key;
+    for (int k = 0; k < 4; ++k)
+    {
+      key += readUnalignBigEndian<char>(buffer.data() + offset);
+      offset++;
+    }
+    auto innerArrayLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+    offset += 2;
+    for (int j = 0; j < innerArrayLen; ++j)
+    {
+      //      auto v = readUnalignBigEndian<std::uint8_t>(buffer.data() + offset);
+      offset += 1;
+    }
+  }
+  auto scanIfLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+  for (int i = 0; i < scanIfLen; ++i)
+  {
+    //    auto ifaceNum = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+    offset += 2;
+    auto ifaceNameLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+    offset += 2;
+    std::string ifaceName(reinterpret_cast<const char*>(buffer.data() + offset), ifaceNameLen);
+    offset += ifaceNameLen;
+  }
+  auto comSettingsLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+
+  for (int i = 0; i < comSettingsLen; ++i)
+  {
+    std::string key;
+    for (int k = 0; k < 4; ++k)
+    {
+      key += readUnalignBigEndian<char>(buffer.data() + offset);
+      offset++;
+    }
+    auto innerArrayLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+    offset += 2;
+    if (key == "EMAC")
+    {
+      std::memcpy(deviceInfo.macAddress.macAddress, buffer.data() + offset, sizeof(deviceInfo.macAddress.macAddress));
+      offset += 6;
+      continue;
+    }
+    if (key == "EIPa")
+    {
+      std::string ipAddr;
+      for (int k = 0; k < 4; ++k)
+      {
+        ipAddr += std::to_string(static_cast<unsigned>(readUnalignBigEndian<char>(buffer.data() + offset)));
+        if (k < 3)
+        {
+          ipAddr += ".";
+        }
+        offset++;
+      }
+      deviceInfo.ipAddress = ipAddr;
+      continue;
+    }
+    if (key == "ENMa")
+    {
+      std::string subNet;
+      for (int k = 0; k < 4; ++k)
+      {
+        subNet += std::to_string(static_cast<unsigned>(readUnalignBigEndian<char>(buffer.data() + offset)));
+        if (k < 3)
+        {
+          subNet += ".";
+        }
+        offset++;
+      }
+      deviceInfo.subNet = subNet;
+      continue;
+    }
+    if (key == "EDGa")
+    {
+      std::string stdGw;
+      for (int k = 0; k < 4; ++k)
+      {
+        stdGw += std::to_string(static_cast<unsigned>(readUnalignBigEndian<char>(buffer.data() + offset)));
+        if (k < 3)
+        {
+          stdGw += ".";
+        }
+        offset++;
+      }
+      continue;
+    }
+    if (key == "EDhc")
+    {
+      //      dhcp = readUnalignBigEndian<std::uint8_t>(buffer.data() + offset);
+      offset += 1;
+      continue;
+    }
+    if (key == "ECDu")
+    {
+      //      auto configTime = readUnalignBigEndian<std::uint32_t>(buffer.data() + offset);
+      offset += 4;
+      continue;
+    }
+    for (int j = 0; j < innerArrayLen; ++j)
+    {
+      //      auto v = readUnalignBigEndian<std::uint8_t>(buffer.data() + offset);
+      offset += 1;
+    }
+  }
+  auto endPointsLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+  offset += 2;
+
+  std::vector<std::uint16_t> ports;
+  for (int i = 0; i < endPointsLen; ++i)
+  {
+    //    auto colaVersion = readUnalignBigEndian<std::uint8_t>(buffer.data() + offset);
+    offset += 1;
+    auto innerArrayLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+    offset += 2;
+    for (int j = 0; j < innerArrayLen; ++j)
+    {
+      std::string key(reinterpret_cast<const char*>(buffer.data() + offset), 4u);
+      offset += 4;
+
+      auto mostInnerArrayLen = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+      offset += 2;
+
+      if (key == "DPNo") // PortNumber [UInt]
+      {
+        auto port = readUnalignBigEndian<std::uint16_t>(buffer.data() + offset);
+        offset += 2;
+        ports.push_back(port);
+      }
+      else
+      {
+        for (int k = 0; k < mostInnerArrayLen; ++k)
+        {
+          //          auto v = readUnalignBigEndian<std::uint8_t>(buffer.data() + offset);
+          offset += 1;
+        }
+      }
+    }
+  }
+  if (!ports.empty())
+  {
+    deviceInfo.port = ports[0];
+  }
+  return deviceInfo;
+}
+
+VisionaryAutoIPScan::VisionaryAutoIPScan(const std::string& serverIP, const std::string& serverNetMask)
+  : m_serverIP(serverIP), m_serverNetMask(serverNetMask)
+{
+}
+
+std::vector<std::uint8_t> VisionaryAutoIPScan::convertIpToBinary(const std::string& address)
+{
+  std::vector<std::uint8_t> ipInts;
+  std::istringstream        ss(address);
+  while (ss)
+  {
+    std::string token;
+    if (!getline(ss, token, '.'))
+      break;
+    ipInts.push_back(static_cast<uint8_t>(std::stoi(token)));
+  }
+  return ipInts;
+}
+
+bool VisionaryAutoIPScan::assign(const MacAddress&                 destinationMac,
+                                 VisionaryAutoIPScan::ProtocolType colaVer,
+                                 const std::string&                ipAddr,
+                                 const std::string&                ipMask,
+                                 const std::string&                ipGateway,
+                                 bool                              dhcp,
+                                 unsigned                          timeout)
+{
+  if (colaVer != ProtocolType::COLA_B && colaVer != ProtocolType::COLA_2)
+  {
+    return false;
+  }
+  ByteBuffer payload;
+  if (colaVer == ProtocolType::COLA_B)
+  {
+    std::string dhcpString = "FALSE";
+    if (dhcp)
+    {
+      dhcpString = "TRUE";
+    }
+    std::string request = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+      "<IPconfig MACAddr=\"" + convertMacToString(destinationMac) + "\">"
+      "<Item key=\"IPAddress\" value=\"" + ipAddr + "\" />"
+      "<Item key=\"IPMask\" value=\"" + ipMask + "\" />"
+      "<Item key=\"IPGateway\" value=\"" + ipGateway + "\" />"
+      "<Item key=\"DHCPClientEnabled\" value=\"" + dhcpString +"\" /></IPconfig>";
+
+    payload.insert(payload.end(), request.c_str(), request.c_str() + request.size());
+  }
+  else if (colaVer == COLA_2)
+  {
+    std::vector<uint8_t> ipAddrVector = convertIpToBinary(ipAddr);
+    payload.insert(payload.end(), ipAddrVector.begin(), ipAddrVector.end());
+
+    std::vector<uint8_t> ipMaskVector = convertIpToBinary(ipMask);
+    payload.insert(payload.end(), ipMaskVector.begin(), ipMaskVector.end());
+
+    std::vector<uint8_t> ipGatewayVector = convertIpToBinary(ipGateway);
+    payload.insert(payload.end(), ipGatewayVector.begin(), ipGatewayVector.end());
+
+    payload.push_back(dhcp);
+  }
+
+  // Init Random generator
+  std::random_device         rd;
+  std::default_random_engine mt(rd());
+  unsigned int               teleIdCounter = mt();
+
+  std::unique_ptr<UdpSocket> pTransport(new UdpSocket());
+
+  if (pTransport->connect(DEFAULT_BROADCAST_ADDR, DEFAULT_PORT) != 0)
+  {
+    return false;
+  }
+
+  // AutoIP Discover Packet
+  ByteBuffer autoIpPacket;
+  autoIpPacket.push_back(0x11); // CMD ip config
+  autoIpPacket.push_back(0x0);  // not defined rfu
+
+  // length of datablock
+  std::uint8_t blockLenght[2];
+  writeUnalignBigEndian(blockLenght, sizeof(blockLenght), static_cast<std::uint16_t>(payload.size()));
+  autoIpPacket.insert(autoIpPacket.end(), blockLenght, blockLenght + 2u);
+
+  // Mac address
+  autoIpPacket.insert(autoIpPacket.end(), destinationMac.macAddress, destinationMac.macAddress + 6u);
+
+  // telegram id
+  unsigned int curtelegramID = teleIdCounter++;
+  std::uint8_t b[4];
+  writeUnalignBigEndian(b, sizeof(b), static_cast<std::uint32_t>(curtelegramID));
+  autoIpPacket.insert(autoIpPacket.end(), b, b + 4u);
+
+  // reserved
+  autoIpPacket.push_back(0x01); // Indicates that telegram is a CoLa Scan telegram
+  autoIpPacket.push_back(0x00);
+
+  // payload
+  autoIpPacket.insert(autoIpPacket.end(), payload.begin(), payload.end());
+
+  pTransport->send(autoIpPacket);
+
+  const std::chrono::steady_clock::time_point startTime(std::chrono::steady_clock::now());
+  while (true)
+  {
+    ByteBuffer                                  receiveBuffer;
+    const std::chrono::steady_clock::time_point now(std::chrono::steady_clock::now());
+    if ((now - startTime) > std::chrono::milliseconds(timeout))
+    {
+      std::cout << __FUNCTION__ << " Timeout" << '\n';
+      break;
+    }
+    if (pTransport->recv(receiveBuffer, 1400) > 16) // 16 bytes minsize
+    {
+      if (receiveBuffer[0] == kRplIpconfig)
+      {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+VisionaryAutoIPScan::MacAddress VisionaryAutoIPScan::convertMacToStruct(const std::string& basicString)
+{
+  std::vector<std::uint8_t> ipInts;
+  std::istringstream        ss(basicString);
+  MacAddress                macAddress{};
+  int                       i = 0;
+  while (ss)
+  {
+    std::string token;
+    size_t      pos;
+    if (!getline(ss, token, ':'))
+      break;
+    macAddress.macAddress[i] = static_cast<std::uint8_t>(std::stoi(token, &pos, 16));
+    i++;
+  }
+  return macAddress;
+}
+
+std::string VisionaryAutoIPScan::convertMacToString(const VisionaryAutoIPScan::MacAddress& macAddress)
+{
+  std::string macAddrString;
+  for (int k = 0; k < 6; ++k)
+  {
+    std::stringstream ss;
+    ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<unsigned>(macAddress.macAddress[k]);
+
+    macAddrString += ss.str();
+    if (k < 5)
+    {
+      macAddrString += ":";
+    }
+  }
+  return macAddrString;
 }
 
 } // namespace visionary
--- src/VisionaryAutoIPScan.h
+++ src/VisionaryAutoIPScan.h
@@ -11,33 +11,89 @@
 
 namespace visionary {
 
+/**
+ * This class is intended to find devices in the network and set a new IP address
+ */
 class VisionaryAutoIPScan
 {
 public:
   static constexpr std::uint16_t DEFAULT_PORT{30718u};
   static const std::string       DEFAULT_BROADCAST_ADDR;
+  static const std::string       DEFAULT_IP_MASK;
+  static const std::string       DEFAULT_GATEWAY;
+  static constexpr bool          DEFAULT_DHCP{false};
+  static constexpr std::uint16_t DEFAULT_TIMEOUT{5000};
+
+  enum ProtocolType
+  {
+    INVALID_PROTOCOL = -1,
+    COLA_A           = 2111,
+    COLA_B           = 2112,
+    COLA_2           = 2122
+  };
+
+  struct MacAddress
+  {
+    std::uint8_t macAddress[6];
+  };
 
   struct DeviceInfo
   {
     std::string   deviceName;
-    std::string   macAddress;
+    MacAddress    macAddress;
     std::string   ipAddress;
     std::string   subNet;
     std::uint16_t port;
+    ProtocolType  protocolType;
   };
 
-  /// <summary>
-  /// Runs an autoIP scan and returns a list of devices
-  /// </summary>
-  /// <returns>A list of devices.</returns>
+  /**
+   * Constructor for the AutoIP scan
+   * @param serverIP ip address of the server which is executing the search
+   * @param serverNetMask netmask of the server which is executing the search
+   */
+  VisionaryAutoIPScan(const std::string& serverIP, const std::string& serverNetMask);
+  /**
+   * Runs an autoIP scan and returns a list of devices
+   * @param timeOut timout of the search in ms
+   * @param broadcastAddress broadcast address for the scan
+   * @param port the port on which to perform the scan
+   * @return a list of devices
+   */
   std::vector<DeviceInfo> doScan(unsigned           timeOut,
                                  const std::string& broadcastAddress = DEFAULT_BROADCAST_ADDR,
                                  std::uint16_t      port             = DEFAULT_PORT);
 
+  /**
+   * Assigns a new ip configuration to a device based on the MAC address
+   * @param destinationMac MAC address of the device on to which to assign the new configuration
+   * @param colaVer cola version of the device
+   * @param ipAddr new ip address of the device
+   * @param ipMask new network mask of the device
+   * @param ipGateway new gateway of the device
+   * @param dhcp true if dhcp should be enabled
+   * @param timout timeout in ms
+   * @return
+   */
+  bool assign(const MacAddress&  destinationMac,
+              ProtocolType       colaVer,
+              const std::string& ipAddr,
+              const std::string& ipMask    = DEFAULT_IP_MASK,
+              const std::string& ipGateway = DEFAULT_GATEWAY,
+              bool               dhcp      = DEFAULT_DHCP,
+              unsigned           timout    = DEFAULT_TIMEOUT);
+
+  static MacAddress                convertMacToStruct(const std::string& basicString);
+  static std::string               convertMacToString(const MacAddress& macAddress);
+
 private:
   using ByteBuffer = std::vector<std::uint8_t>;
+  std::string m_serverIP;
+  std::string m_serverNetMask;
 
-  DeviceInfo parseAutoIPXml(std::stringstream& rStringStream);
+  DeviceInfo                       parseAutoIPXml(std::stringstream& rStringStream);
+  DeviceInfo                       parseAutoIPBinary(const ByteBuffer& receivedBuffer);
+  static std::vector<std::uint8_t> convertIpToBinary(const std::string& address);
 };
 
 } // namespace visionary
